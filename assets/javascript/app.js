a2e8e4e82483d53908e84a851de77f616b41bcf1
Brett Scottini <bscottini1@gmail.com>
2019-07-28 15:04:56 -0400

    All questions complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,23 @@
-let  number = 120;
-
-window.onload = function run (){
-    interval = setInterval(decrement, 1000);
+$(document).ready(function () {
+    // let n = Date.now();
+    // let x = n + (1000*60*2);
+    // let timerSec = ((x-n)/1000);
+    // let runTime = 
+    // function decrement(){
+    //     timerSec--;
+    //     setInterval(decrement), 1000;
+    // }
+    // console.log(timerSec);
+    let number = 120;
+    let numRun= run
+    let intervalID;
+    function run() {
+        function decrement() {
+            number--;
+        intervalID = setInterval(decrement, 1000);
+    }
 }
+    $("#start").on("click", run);
+    $("#show-number").html("<h2>" + number + "</h2>");
 
-function decrement (){
-    number--;
-}
-
-$("#show-number").html("<h2>" + number + "</h2");
-// if number === {
-//     alert (You ran out of time!);
-// }
-
-
-
-
+});
