27f755e2d7069ee5b17a1483f1049841d6dae09b
Brett Scottini <bscottini1@gmail.com>
2019-07-29 22:15:38 -0400

    timer stops!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
-    let number = 5;
+    
     let intervalID;
+
     function run() {
+        let number = 5;
+        intervalID = setInterval(decrement, 1000);
+
         function decrement() {
             number--;
             $("#show-number").html("<h2>" + number + "</h2>");
+            if (number === 0) {
+            stop();
         }
-        intervalID = setInterval(decrement, 1000);
-        }
+      }
+    }
+
     window.onload = run();
     
-    if (number === 0) {
-        stop();
-    }
     function stop(){
     clearInterval(intervalID);
     }
