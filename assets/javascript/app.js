e1308f8adeb4aad34c77cf2d0b3e362ec04c7c18
Brett Scottini <bscottini1@gmail.com>
2019-07-30 20:40:40 -0400

    hide/show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,48 @@
 $(document).ready(function () {
-    
-    let intervalID;
 
-    function run() {
-        let number = 5;
-        intervalID = setInterval(decrement, 1000);
+  let intervalID;
 
-        function decrement() {
-            number--;
-            $("#show-number").html("<h2>" + number + "</h2>");
-            if (number === 0) {
-            stop();
-        }
+  function run() {
+    let number = 15;
+    intervalID = setInterval(decrement, 1000);
+    $("header2").hide(0);
+
+    function decrement() {
+      number--;
+      $("#show-number").html("<h2>" + number + "</h2>");
+      if (number === 0) {
+        stop();
       }
     }
+  }
 
-    window.onload = run();
-    
-    function stop(){
+  window.onload = run();
+
+  function stop() {
     clearInterval(intervalID);
-    }
+    //display results
+  }
 
- });
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+    $("header1").hide();
+  })
+  
+  // $("#submit").on("click", function(){
+  //   $("header").hide();
+  // })
+  
+
+
+  correctAnswers = [2, 1, 4, 3, 1, 4, 1, 3, 2]
+
+  //const radioValue = $("input[name='question1']:checked)".val();
+  // function check(){
+  // }
+
+  const radioValue = $("input[name='question1']:checked").val();
+  //const radioValue = $("input[type='radio'][name='question1']:checked").val();
+  //const radioValue = document.getElementsByName("question1").value;
+
+  console.log(radioValue);
+});
